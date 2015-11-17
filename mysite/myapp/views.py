e76bb670cc49c683e77e330572aa6b1ab2bd243a
vish4 <vish.n412@gmail.com>
2015-11-17 18:52:14 -0500

    Update views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,24 +1,12 @@
 from django.http import HttpResponse
-#import redis
-#redis_POOL=redis.ConnectionPool(host='104.131.206.44', port=6379,db=0)
-#redis_ins = redis.StrictRedis(host='localhost', port=6379,db=0)
-#redis_ins.set('background', 1)
-
-# def getVariable(variable_name):
-#     my_server = redis.Redis(connection_pool=redis_POOL)
-#     response = my_server.get(variable_name)
-#     return response
-
-# value=getVariable('background')
-# print (value)
-#def hello(request):
-#	val=redis_ins.get('background')
-#	if val == "red":
-#		return HttpResponse("<body bgcolor='#FF0000'> <p>Hello, world</p> </body>") 
-#	else:
-#		return HttpResponse("<body bgcolor='#00ff40'> <p>Hello, world</p> </body>") 
-
-# Changing color from Red to Green => 00ff40
+import redis
+import os
+host=os.getenv("DB_PORT_6379_TCP_ADDR")
+redis_ins = redis.StrictRedis(host=host, port=6379)
 
 def hello(request):
-		return HttpResponse("<body bgcolor='#00ff40'> <p>Hello, world</p> </body>")
+	val=redis_ins.get('background')
+	if val == "red":
+		return HttpResponse("<body bgcolor='#FF0000'> <p>Hello, world</p> </body>") 
+	else:
+		return HttpResponse("<body bgcolor='#00ff40'> <p>Hello, world</p> </body>") 
